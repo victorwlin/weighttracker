4422c4a49491066bf5fc3a5bca081246cd6d6bc1
victorwlin <57220247+victorwlin@users.noreply.github.com>
2020-05-01 14:31:51 +0800

    Add files via upload

---
+++ docs/precache-manifest.2f4170815a44d28a4a1cf72972fd4777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bdcb2efb886fc19fc9e9e495ba7e42",
+    "url": "/weighttracker/index.html"
+  },
+  {
+    "revision": "d876e3d2c5e4c8c7b9ed",
+    "url": "/weighttracker/static/js/2.b604b9d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weighttracker/static/js/2.b604b9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108b4c7ce884146209fa",
+    "url": "/weighttracker/static/js/main.daeab4a0.chunk.js"
+  },
+  {
+    "revision": "253e174a4f3ae47a719f",
+    "url": "/weighttracker/static/js/runtime-main.6022443d.js"
+  }
+]);
